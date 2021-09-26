ee1da5818d3f46cde80c31524036d9c5bfa5e2b6
rishav-jha-mech <rishav.rj74@gmail.com>
2021-09-27 00:08:01 +0530

    Fixed Encoding Issue

--- SpamBot.py
+++ SpamBot.py
@@ -1,10 +1,10 @@
 import pyautogui, time  # Importing pyautogui and time modules
 
-time.sleep(5)           # Delaying the program for 5s, this is the time when you have to click on the textbox of whatsapp/ anyother messaging app
+time.sleep(3)           # Delaying the program for 5s, this is the time when you have to click on the textbox of whatsapp/ anyother messaging app
 
-f=open("./texts" , 'r')   # Here the path and name of the file (example "texts" or "./TonyKakkar/AnySongName") is given and 'r' means read
+f=open("./texts" , 'r',encoding="utf8")   # Here the path and name of the file (example "texts" or "./TonyKakkar/AnySongName") is given and 'r' means read
 
 for x in f:                     # A for loop to print all the words
     pyautogui.typewrite(x)
-    time.sleep(0.2)             # Time delay given between sending of two consecutive messages if this time is very small your system may hang
+    # time.sleep(0.2)             # Time delay given between sending of two consecutive messages if this time is very small your system may hang
     pyautogui.press("enter")    
